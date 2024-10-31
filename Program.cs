5825926dc417e7c8598d6dd8d03ad25f1734f71d
nazarkobryn <kobrinnazar05@gmail.com>
2024-10-30 20:26:03 -0400

    add func

--- Program.cs
+++ Program.cs
@@ -1,26 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
 
 using System;
 using System.Globalization;
 using System.Collections.Generic;
+using System.ComponentModel;
 
-namespace ConsoleApp1
-
+namespace LearnCSharp
 {
     class Program
     {
         static void Main()
         {
+            EvenOdd();
+            Multiply();
+        }
 
-
+        static void EvenOdd()
+        {
             int stringLength = Int32.TryParse(Console.ReadLine(), out stringLength) ? stringLength : 0;
 
             Dictionary<string, List<string>> result = new Dictionary<string, List<string>>();
 
             result["even"] = new List<string>();
             result["odd"] = new List<string>();
-
 
             for (int i = 0; i < stringLength; i++)
             {
@@ -37,18 +39,36 @@ namespace ConsoleApp1
             int evenCount = result["even"].Count;
             int oddCount = result["odd"].Count;
 
-            // Console.WriteLine("Even numbers count: " + evenCount);
-            // Console.WriteLine("Odd numbers count: " + oddCount);
-
+            Console.WriteLine("Even numbers count: " + evenCount);
+            Console.WriteLine("Odd numbers count: " + oddCount);
 
             int[] evenNumbers = result["even"].ConvertAll(int.Parse).ToArray();
             int[] oddNumbers = result["odd"].ConvertAll(int.Parse).ToArray();
 
-            // Console.WriteLine("Even numbers len: " + evenNumbers.Length);
-            // Console.WriteLine("Odd numbers len: " + oddNumbers.Length);
+            Console.WriteLine("Even numbers len: " + evenNumbers.Length);
+            Console.WriteLine("Odd numbers len: " + oddNumbers.Length);
+        }
 
+        static void Multiply()
+        {
+            int number = 7;
+            int countOfNumber = 5;
 
+            int startNumber = number;
+            List<int> result = new List<int>();
+            int[] resultArray = new int[countOfNumber];
+
+            for (int i = 0; i < countOfNumber; i++)
+            {
+                result.Add(startNumber);
+                resultArray[i] = startNumber;
+                startNumber += number;
+            }
+
+            Console.WriteLine("Result: " + string.Join(", ", result));
+            Console.WriteLine("Result array: " + string.Join(", ", resultArray));
         }
     }
 }
+
 
